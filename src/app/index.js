37eda34cc010487b029fbb2ed31a31580cc96428
mahmoud717 <mahmoudmohammad717@gmail.com>
2020-12-02 11:40:40 +0200

    added event listeners for all tabs

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,20 @@
 import '../assets/style/main.scss';
+import switchToHome from './utils/home';
+import switchToMenu from './utils/menu';
+import switchToContacts from './utils/contacts';
+
+(function loadHomeDefault() {
+  switchToHome();
+}());
+(function addEventListeners() {
+  const tabs = document.querySelector('.tabs');
+  tabs.querySelector('.home-tab').addEventListener('click', () => {
+    switchToHome();
+  });
+  tabs.querySelector('.menu-tab').addEventListener('click', () => {
+    switchToMenu();
+  });
+  tabs.querySelector('.contacts-tab').addEventListener('click', () => {
+    switchToContacts();
+  });
+}());
